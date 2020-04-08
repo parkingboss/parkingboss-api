5b8819b67981e3d4d717f06e7775f6223a151cb6
Chris Pfohl <christopher.pfohl@gmail.com>
2020-04-08 14:52:40 -0400

    Move type defs from svelte-utils to here

--- src/api/payloads/Payload.ts
+++ src/api/payloads/Payload.ts
@@ -1,10 +1,23 @@
+export type IdString = string;
+
+export type TypeString = string;
+
+export interface Item {
+  id: IdString;
+  generated?: string | Date;
+  updated?: string | Date;
+}
+
+export type ItemAttachments = Record<IdString, TypeString>;
+
+export type Items = Record<IdString, Item>;
+export type Attachments = Record<IdString, ItemAttachments>;
+
 export interface Payload {
   generated: string;
   server: string;
   viewpoint: string;
   version?: string;
-  items: Record<string, unknown>;
-  attachments: {
-    items: Record<string, Record<string, string>>;
-  };
+  items: Items;
+  attachments?: Attachments;
 }
