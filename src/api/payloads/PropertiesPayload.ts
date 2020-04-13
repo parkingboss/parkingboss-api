99ee459efded7ed2374c015097524b0e8bae6487
Nathan LeMesurier <nathan@parkingboss.com>
2020-04-13 15:29:01 -0400

    extend payload

--- src/api/payloads/PropertiesPayload.ts
+++ src/api/payloads/PropertiesPayload.ts
@@ -1,4 +1,6 @@
-export interface PropertiesPayload {
+import { Payload } from "./Payload";
+
+export interface PropertiesPayload extends Payload {
   properties: {
     count: number;
     item?: string;
