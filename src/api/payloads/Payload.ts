532e1f3dca066aa9d08e2a1de1d87c0b6d790c45
Joe Pfohl <jpfohl@parkingboss.com>
2020-04-14 15:16:20 -0400

    update payload typings

--- src/api/payloads/Payload.ts
+++ src/api/payloads/Payload.ts
@@ -1,23 +1,29 @@
 export type IdString = string;
-
 export type TypeString = string;
 
-export interface Item {
+export interface Entity {
   id: IdString;
+  type: TypeString;
   generated?: string | Date;
   updated?: string | Date;
 }
 
-export type ItemAttachments = Record<IdString, TypeString>;
+export type Attachment = Record<IdString, TypeString>;
 
-export type Items = Record<IdString, Item>;
-export type Attachments = Record<IdString, ItemAttachments>;
+export type Items<T> = Record<IdString, T>;
+
+export type EntityItems = Items<Entity>;
+export type AttachmentItems = Items<Attachment>;
+
+export interface Attachments {
+  items: AttachmentItems;
+}
 
 export interface Payload  {
   generated: string;
   server: string;
   viewpoint: string;
   version?: string;
-  items: Items;
+  items: EntityItems;
   attachments?: Attachments;
 }
